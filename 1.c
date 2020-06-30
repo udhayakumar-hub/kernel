05234c7e1480d449310490a85b37f6914b499c2d
udhaya <udhayakumar0405@outlook.com>
2020-06-30 11:29:38 +0530

    1.c commit

---
+++ 1.c
@@ -0,0 +1,12 @@
+#include<stdio.h>
+#include<unistd.h>
+int main()
+{
+	printf("hello -man how are you\n");
+	sleep(10);
+	printf("\nsleepcompleted\n");
+	sleep(15);
+	printf("completed succesfully\n");
+	sleep(120);
+
+}
